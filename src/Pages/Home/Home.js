c6c449e7d861126a4f933830bb8faa80f9749ba8
NH-Rifat <rifatr729@gmail.com>
2022-05-20 19:10:26 +0600

    Banner service appointment review contact and footer section added successfully

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Contact from './Contact';
+import Footer from './Footer';
+import Info from './Info';
+import MakeAppointment from './MakeAppointment';
+import Services from './Services';
+import Testimonials from './Testimonial';
 
 const Home = () => {
   return (
     <div className='container mx-auto'>
       <Banner></Banner>
+      <Info></Info>
+      <Services></Services>
+      <MakeAppointment></MakeAppointment>
+      <Testimonials></Testimonials>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,22 @@
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    screens: {
-      xxsm: '315px',
-      xsm: '410px',
-      sm: '550px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      xxl: '1490px',
-      '3xl': '1720px',
-    },
-    extend: {
-      colors: {
-        primary: '#0FCFEC',
-        secondary: '#19D3AE',
-        accent: '#3A4256',
-        neutral: '#3d4451',
-        'base-100': '#ffffff',
-        brown: {
-          50: '#fdf8f6',
-          100: '#f2e8e5',
-          200: '#eaddd7',
-          300: '#e0cec7',
-          400: '#d2bab0',
-          500: '#bfa094',
-          600: '#a18072',
-          700: '#977669',
-          800: '#846358',
-          900: '#43302b',
-        },
-        red: {
-          50: '#b5365a',
-          100: '#c72653',
-          200: '#b0123e',
-          300: '#d6134b',
-          400: '#d2bab0',
-          500: '#d6134b',
-          600: '#a18072',
-          700: '#977669',
-          800: '#846358',
-          900: '#43302b',
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
         },
       },
-    },
+      "dark",
+      "cupcake",
+    ],
   },
-  plugins: [require('daisyui')],
-};
+  plugins: [require("daisyui")],
+}
